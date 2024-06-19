afff12a12fdd803b6e50074c697eeadc419ecafd
Air3movies <107802871+Air3movies@users.noreply.github.com>
2024-06-19 13:08:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,11 @@
 // JavaScript code will be added here for interactivity
-history.replaceState(null, null, '/');
+history.replaceState(null, null, '');
+
+// Scroll text horizontally
+function scrollText() {
+    const banner = document.getElementById('scrolling-text-container');
+    banner.style.animation = 'scroll-left 20s linear infinite';
+}
+
+// Call scrollText function when the DOM is loaded
+document.addEventListener('DOMContentLoaded', scrollText);
